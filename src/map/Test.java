c328c27db6a848165e43f06ea2fc01edd2bd6dae
Callum <callumdmay@gmail.com>
2015-04-04 17:29:05 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/critters/Critter$direction.class
    	bin/critters/Critter.class
    	bin/game/MapSelectScreen.class
    	bin/game/PlayScreen.class
    	bin/towers/Tower.class

--- src/map/Test.java
+++ src/map/Test.java
@@ -7,12 +7,9 @@ import java.util.ArrayList;
 public class Test {
 
 	public static void main(String[] args) throws IOException{
-	
-		MapEditor map = new MapEditor(5, 5, "(0,0) (4,0)", "Test");
 
-		map.listFilesforFolder();
-		System.out.println(map.getListofFiles());
-		
-		System.out.println();
+		LoadFile loading = new LoadFile();
+		System.out.println(loading.loadFile(loading.getListofFiles().get(0)));
+		System.out.println(loading.getAllMap());
 	}
 }
